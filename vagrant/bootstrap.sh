82f2e5bf4e999b0a7acaad6020a6f984d73d4642
Tomasz <tomasz.rozdzynski@codete.co>
2016-06-28 17:27:41 +0200

    vagrant update

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-apt-get install -y mc joe git multitail screen nmap
+apt-get install -y mc joe git multitail screen nmap python-pip
 
 curl -sSL https://get.docker.com/ | sh
 
 sudo usermod -aG docker vagrant
+
+pip install docker-compose
