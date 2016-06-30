fd6c93f9150a2c3e898aaa73211d611cffe19016
Tomasz <tomasz.rozdzynski@codete.co>
2016-06-30 17:13:38 +0200

    nameserver fix

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get install -y mc joe git multitail screen nmap python-pip
+sudo apt-get install -y mc joe git multitail screen nmap python-pip htop iotop jnettop elinks
+
 
 curl -sSL https://get.docker.com/ | sh
 
 sudo usermod -aG docker vagrant
 
 sudo pip install docker-compose
+
+
