094dcd0420a45a8827ae3e178a38245a201f8878
Tomasz <tomasz.rozdzynski@codete.co>
2016-06-30 17:21:37 +0200

    nameserver fix, docker-machine install

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -15,3 +15,5 @@ sudo usermod -aG docker vagrant
 sudo pip install docker-compose
 
 
+curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine
+
